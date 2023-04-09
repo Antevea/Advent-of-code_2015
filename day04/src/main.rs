b4e96657610258f09adb3e586819569138df22df
Antevea <Tosvering@gmail.com>
2023-04-09 10:57:35 +0500

    Solution for AOC day 4

--- day04/src/main.rs
+++ day04/src/main.rs
@@ -5,11 +5,9 @@ fn part1() {
     let filepath = "puzzle";
     let input_str = read_to_string(filepath).expect(&format!("Error: File {} not found!", filepath));
 
-    let mut i: u64 = 0;
     let mut _digest: md5::Digest = md5::Digest::from(md5::Context::new());
-    while i < u64::MAX {
+    for i in 0..u64::MAX {
         let secret_key = input_str.trim_end().to_string().clone() + i.to_string().as_str();
-        i += 1;
         _digest = md5::compute(secret_key.as_bytes());
         let digest_str = format!("{:x}", _digest);
         if digest_str.starts_with("00000") {
@@ -22,7 +20,27 @@ fn part1() {
     println!("Unable to find value with u64 MAX {}", u64::MAX);
 }
 
+fn part2() {
+    // let filepath = "sample";
+    let filepath = "puzzle";
+    let input_str = read_to_string(filepath).expect(&format!("Error: File {} not found!", filepath));
+
+    let mut _digest: md5::Digest = md5::Digest::from(md5::Context::new());
+    for i in 0..u64::MAX {
+        let secret_key = input_str.trim_end().to_string().clone() + i.to_string().as_str();
+        _digest = md5::compute(secret_key.as_bytes());
+        let digest_str = format!("{:x}", _digest);
+        if digest_str.starts_with("000000") {
+            println!("{}", secret_key.clone());
+            return;
+        } else {
+            continue;
+        }
+    }
+    println!("Unable to find value with u64 MAX {}", u64::MAX);
+}
+
 fn main() {
     part1();
-    // part2();
+    part2();
 }
