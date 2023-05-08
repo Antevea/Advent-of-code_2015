f07f252d9ca17676b3d5342590385c9b0222bf9b
Antevea <Tosvering@gmail.com>
2023-05-08 12:34:07 +0500

    Fist part of solution for AOC 2015 day 8

---
+++ day08/src/main.rs
@@ -0,0 +1,46 @@
+use std::fs::read_to_string;
+
+fn str_size_diff(s: &str) -> u32 {
+    let mut escape_count = 0_u32;
+    let s_vec = s.chars().collect::<Vec<char>>();
+    let mut i = 1;
+    while i < s_vec.len() - 1 {
+        escape_count += 1;
+        if s_vec[i] == '\\' {
+            if s_vec[i + 1] == 'x' {
+                i += 4;
+            } else {
+                i += 2;
+            }
+        } else {
+            i += 1;
+        }
+    }
+
+    s.len() as u32 - escape_count
+}
+
+fn part1() {
+    // let filepath = "sample";
+    let filepath = "puzzle";
+    let input_str = read_to_string(filepath).expect(&format!("Error: File {} not found!", filepath));
+
+    let mut file_size_diff = 0_u32;
+
+    for line in input_str.lines() {
+        file_size_diff += str_size_diff(line);
+    }
+
+    println!("File size difference: {}", file_size_diff);
+}
+
+/* fn part2() {
+    let filepath = "sample";
+    // let filepath = "puzzle";
+    let input_str = read_to_string(filepath).expect(&format!("Error: File {} not found!", filepath));
+} */
+
+fn main() {
+    part1();
+    // part2();
+}
