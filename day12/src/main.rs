aecd769309303d3308d241c05e8152a831a51e44
Antevea <Tosvering@gmail.com>
2023-07-21 00:08:42 +0500

    Solution for AOC 2015 day 12

--- day12/src/main.rs
+++ day12/src/main.rs
@@ -1,4 +1,5 @@
 use std::fs::read_to_string;
+use serde_json::Value;
 
 fn sum_of_all_numbers(json_str: &String) -> i64 {
     let mut str_iter = json_str.chars().into_iter();
@@ -26,6 +27,36 @@ fn sum_of_all_numbers(json_str: &String) -> i64 {
     sum
 }
 
+fn sum_redless(val: &Value) -> Option<i64> {
+    match val {
+        Value::Null | Value::Bool(_) => Some(0),
+        Value::String(s) => {
+            if s == "red" {
+                return None;
+            } else {
+                return Some(0);
+            }
+        },
+        Value::Number(num) => Some(num.as_i64().unwrap()),
+        Value::Array(arr) => Some(arr
+                                  .iter()
+                                  .map(|ent| sum_redless(ent).unwrap_or(0))
+                                  .sum()),
+        Value::Object(obj) => {
+            let (vec_somes, vec_nones): (Vec<_>, Vec<_>) = obj
+                .values()
+                .map(|ent| sum_redless(ent))
+                .partition(Option::is_some);
+
+            if vec_nones.is_empty() {
+                Some(vec_somes.iter().map(|ent| ent.unwrap()).sum())
+            } else {
+                return Some(0);
+            }
+        },
+    }
+}
+
 fn part1() {
     // let filepath = "sample";
     let filepath = "puzzle";
@@ -34,13 +65,16 @@ fn part1() {
     println!("Sum of all numbers in json file: {}", sum_of_all_numbers(&input_str));
 }
 
-/* fn part2() {
-    let filepath = "sample";
-    // let filepath = "puzzle";
+fn part2() {
+    // let filepath = "sample";
+    let filepath = "puzzle";
     let input_str = read_to_string(filepath).expect(&format!("Error: File {} not found!", filepath));
-} */
+
+    let json: Value = serde_json::from_str(input_str.as_str()).expect(&format!("Error: Can't parse json file {}", filepath));
+    println!("{}", sum_redless(&json).expect(&format!("Error: Can't count readless numbers in file {}", filepath)));
+}
 
 fn main() {
     part1();
-    // part2();
+    part2();
 }
