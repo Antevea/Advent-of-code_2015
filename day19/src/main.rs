354d2a4b11291f3d3ab0b3c0de08b237ff6b4725
Antevea <Tosvering@gmail.com>
2023-12-15 00:08:04 +0500

    First part of the solution for Advent of Code 2015 day 19

---
+++ day19/src/main.rs
@@ -0,0 +1,41 @@
+use std::fs::read_to_string;
+use std::collections::{HashMap, HashSet};
+
+fn part1() {
+    // let filepath = "sample";
+    let filepath = "puzzle";
+    let input_str = read_to_string(filepath).expect(&format!("Error: File {} not found!", filepath));
+
+    let mut combinations: HashMap<String, String> = HashMap::new();
+    let mut molecule = String::new();
+    let mut crafted_molecules: HashSet<String> = HashSet::new();
+
+    for line in input_str.lines() {
+        if let Some((left, right)) = line.split_once(" => ") {
+            combinations.insert(right.to_string(), left.to_string());
+        } else {
+            molecule = line.to_string();
+        }
+    }
+
+    for (key, val) in &combinations {
+        for mol in molecule.match_indices(val) {
+            let (left, right) = molecule.split_at(mol.0);
+            let right = right.to_string().split_off(val.len());
+            crafted_molecules.insert(format!("{left}{key}{right}"));
+        }
+    }
+
+    println!("Part 1: {}", crafted_molecules.len());
+}
+
+/* fn part2() {
+    let filepath = "sample";
+    // let filepath = "puzzle";
+    let input_str = read_to_string(filepath).expect(&format!("Error: File {} not found!", filepath));
+} */
+
+fn main() {
+    part1();
+    // part2();
+}
