d581f1ffa68eb7c5fc83b4c63a851d1ddfbccc10
Antevea <Tosvering@gmail.com>
2023-12-15 16:35:28 +0500

    Solution for Advent of Code 2015 day 19

--- day19/src/main.rs
+++ day19/src/main.rs
@@ -29,13 +29,43 @@ fn part1() {
     println!("Part 1: {}", crafted_molecules.len());
 }
 
-/* fn part2() {
-    let filepath = "sample";
-    // let filepath = "puzzle";
+fn part2() {
+    // let filepath = "sample";
+    let filepath = "puzzle";
     let input_str = read_to_string(filepath).expect(&format!("Error: File {} not found!", filepath));
-} */
+
+    let mut combinations: HashMap<String, String> = HashMap::new();
+    let mut molecule = String::new();
+    let mut count = 0;
+
+    for line in input_str.lines() {
+        if let Some((left, right)) = line.split_once(" => ") {
+            combinations.insert(right.to_string(), left.to_string());
+        } else {
+            molecule = line.to_string();
+        }
+    }
+
+    loop {
+        let mut brek = true;
+        for (key, val) in &combinations {
+            if let Some(pos) = molecule.find(key) {
+                let (left, right) = molecule.split_at(pos);
+                let right = right.to_string().split_off(key.len());
+                molecule = format!("{left}{val}{right}");
+                brek = false;
+                count += 1;
+            }
+        }
+        if brek {
+            break;
+        }
+    }
+
+    println!("Part 2: {}", count);
+}
 
 fn main() {
     part1();
-    // part2();
+    part2();
 }
