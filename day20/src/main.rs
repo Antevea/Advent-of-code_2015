2684462e384c98a8a70d0e84d6bbdbbd2b35934e
Antevea <Tosvering@gmail.com>
2023-12-17 23:26:46 +0500

    Solution for Advent of Code 2015 day 20

--- day20/src/main.rs
+++ day20/src/main.rs
@@ -19,13 +19,28 @@ fn part1() {
     }
 }
 
-/* fn part2() {
-    let filepath = "sample";
-    // let filepath = "puzzle";
+fn part2() {
+    // let filepath = "sample";
+    let filepath = "puzzle";
     let input_str = read_to_string(filepath).expect(&format!("Error: File {} not found!", filepath));
-} */
+
+    let presents_target = input_str.trim().parse::<u64>().unwrap();
+    let limit = 1000000;
+    let mut houses = vec![0u64; limit];
+
+    'outer : for elf in 1..limit {
+        let elf_visited_num = limit.min(elf * 50);
+        for i in (elf..elf_visited_num).step_by(elf) {
+            houses[i] += elf as u64 * 11;
+            if houses[i] >= presents_target {
+                println!("Part 2: {}", i);
+                break 'outer;
+            }
+        }
+    }
+}
 
 fn main() {
     part1();
-    // part2();
+    part2();
 }
